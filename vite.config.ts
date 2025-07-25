342293f650a546934afad78286ebda22e2312da8
Connor <connorturlan@gmail.com>
2025-07-25 17:22:38 +0930

    modifying config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        enabled: false, // Only enable in development
+        buildMode: true, // Ensure it runs during build in development
+      },
+      eslint: {
+        enabled: false, // Disable eslint if you want
+      },
+    }),
+  ],
 });
