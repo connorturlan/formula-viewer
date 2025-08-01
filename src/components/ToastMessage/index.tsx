ea783112db7f8bba073aec37355b76979183aa12
Connor <connorturlan@gmail.com>
2025-08-01 09:44:28 +0930

    toast is cooked

--- src/components/ToastMessage/index.tsx
+++ src/components/ToastMessage/index.tsx
@@ -7,50 +7,46 @@ export interface ToastMessageEvent {
 }
 
 export const ToastMessage = () => {
-  const [messages, setMessages] = useState<string[]>([]);
   const [oldestMessage, setOldest] = useState<number>(0);
   const [agingMessage, setAging] = useState<number>(0);
 
-  const oldestMessageIndex = useRef(0);
-  const agingMessageIndex = useRef(0);
   const allMessages = useRef<string[]>([]);
 
   const handleIncommingMessage = (message: string) => {
     console.log("new message: ", message);
     allMessages.current.push(message);
-    setMessages(allMessages.current.slice());
+
+    const messageIndex = allMessages.current.length;
 
     setTimeout(() => {
-      agingMessageIndex.current += 1;
+      setAging(messageIndex);
+      setTimeout(() => {
+        setOldest(messageIndex);
+      }, 500);
     }, 3_000);
-    setTimeout(() => {
-      oldestMessageIndex.current += 1;
-      setMessages(
-        allMessages.current.slice(
-          oldestMessageIndex.current
-        )
-      );
-    }, 4_000);
   };
 
   const handleInfoMessage = (event: any) => {
-    handleIncommingMessage(event.message!);
+    handleIncommingMessage(`[INFO]${event.message!}`);
   };
   const handleErrorMessage = (event: any) => {
-    handleIncommingMessage(event.message!);
+    handleIncommingMessage(`[ERROR]${event.message!}`);
   };
 
   UseSub("InfoMessage", handleInfoMessage);
   UseSub("ErrorMessage", handleErrorMessage);
 
   useEffect(() => {
-    console.warn("new message has arrived", messages);
-    console.log(
-      oldestMessageIndex,
-      agingMessageIndex,
-      messages.length
+    console.warn(
+      "new message has arrived",
+      allMessages.current
     );
-  }, [messages]);
+    console.log(
+      oldestMessage,
+      agingMessage,
+      allMessages.current.length
+    );
+  }, [allMessages.current]);
 
   const ref = useRef(0);
   const count = useRef(0);
@@ -59,7 +55,7 @@ export const ToastMessage = () => {
     if (ref.current > 1) return;
     setInterval(() => {
       count.current += 1;
-      if (count.current > 10) return;
+      // if (count.current > 10) return;
       handleIncommingMessage(
         "test" + new Date().toISOString()
       );
@@ -68,23 +64,31 @@ export const ToastMessage = () => {
 
   return (
     <div className={styles.Container}>
-      {messages.map((message, index) => {
-        return (
-          <p
-            key={`${message}`}
-            className={`${styles.Bubble} ${
-              index <
-              allMessages.current.length -
-                agingMessageIndex.current -
-                1
-                ? styles.BubbleHide
-                : styles.BubbleShow
-            }`}
-          >
-            {message}
-          </p>
-        );
-      })}
+      {allMessages.current
+        .slice(oldestMessage)
+        .map((message, index) => {
+          let messageClass = styles.BubbleInfo;
+
+          if (message.startsWith("[INFO]"))
+            messageClass = styles.BubbleInfo;
+          else if (message.startsWith("[WARN]"))
+            messageClass = styles.BubbleWarn;
+          else if (message.startsWith("[ERROR]"))
+            messageClass = styles.BubbleError;
+
+          return (
+            <p
+              key={`${index + oldestMessage} ${message}`}
+              className={`${styles.Bubble} ${
+                index + oldestMessage < agingMessage
+                  ? styles.BubbleHide
+                  : styles.BubbleShow
+              } ${messageClass}`}
+            >
+              {message}
+            </p>
+          );
+        })}
     </div>
   );
 };
